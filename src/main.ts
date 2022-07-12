ea8443d12bc17ba61e1e99b7d54fe51690718c36
Sergey Kruptsov <serega.by@inbox.ru>
2022-07-12 16:52:59 +0300

    init: init project and add swagger

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,36 @@
+// import { NestFactory } from '@nestjs/core';
+// import { SwaggerModule, DocumentBuilder } from '@nestjs/swagger';
+// import { AppModule } from './app.module';
+
+// async function bootstrap() {
+//   const app = await NestFactory.create(AppModule);
+//   const config = new DocumentBuilder()
+//     .setTitle('Cats example')
+//     .setDescription('The cats API description')
+//     .setVersion('1.0')
+//     .addTag('cats')
+//     .build();
+//   const document = SwaggerModule.createDocument(app, config);
+//   SwaggerModule.setup('doc', app, document);
+//   await app.listen(5000);
+// }
+// bootstrap();
+
 import { NestFactory } from '@nestjs/core';
+import { SwaggerModule } from '@nestjs/swagger';
+import { readFile } from 'fs/promises';
+import { dirname, join } from 'path';
+import { parse } from 'yaml';
 import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  await app.listen(4000);
+  const rootDirname = dirname(__dirname);
+  const DOC_API = await readFile(join(rootDirname, 'doc', 'api.yaml'), 'utf-8');
+  const document = parse(DOC_API);
+
+  SwaggerModule.setup('doc', app, document);
+  await app.listen(5000);
 }
+
 bootstrap();
